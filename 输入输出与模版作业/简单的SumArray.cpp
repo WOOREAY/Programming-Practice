92aba0bc090427e1041d762ab6b246983b83c4dc
WOOREAY <2200012115@stu.pku.edu.cn>
2025-03-15 16:18:52 +0800

    输入输出与模版作业

---
+++ 输入输出与模版作业/简单的SumArray.cpp
@@ -0,0 +1,55 @@
+/*
+A:简单的SumArray
+查看提交统计提问
+总时间限制: 1000ms 内存限制: 65536kB
+描述
+填写模板 PrintArray,使得程序输出结果是： TomJackMaryJohn 10 不得编写SumArray函数
+
+#include <iostream>
+#include <string>
+using namespace std;
+template <class T>
+T SumArray(
+// 在此处补充你的代码
+}
+int main() {
+	string array[4] = { "Tom","Jack","Mary","John"};
+	cout << SumArray(array,array+4) << endl;
+	int a[4] = { 1, 2, 3, 4};  //提示：1+2+3+4 = 10
+	cout << SumArray(a,a+4) << endl;
+	return 0;
+}
+输入
+无
+输出
+TomJackMaryJohn
+10
+样例输入
+无
+样例输出
+TomJackMaryJohn
+10
+来源
+Guo Wei
+查看 提交 统计 提问
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+template <class T>
+T SumArray(
+// 在此处补充你的代码
+T *begin, T *end) {
+    T sum;
+    for (T *p = begin; p != end; p++) {
+        sum += *p;
+    }
+    return sum;
+}
+int main() {
+	string array[4] = { "Tom","Jack","Mary","John"};
+	cout << SumArray(array,array+4) << endl;
+	int a[4] = { 1, 2, 3, 4};  //提示：1+2+3+4 = 10
+	cout << SumArray(a,a+4) << endl;
+	return 0;
+}
---
+++ 输入输出与模版作业/简单的foreach.cpp
@@ -0,0 +1,102 @@
+/*
+B:简单的foreach
+查看提交统计提问
+总时间限制: 1000ms 内存限制: 65536kB
+描述
+编写MyForeach模板，使程序按要求输出 不得编写 MyForeach函数
+
+#include <iostream>
+#include <string>
+using namespace std;
+// 在此处补充你的代码
+void Print(string s)
+{
+	cout << s;
+}
+void Inc(int & n)
+{
+	++ n;
+}
+string array[100];
+int a[100];
+int main() {
+	int m,n;
+	while(cin >> m >> n) {
+		for(int i = 0;i < m; ++i)
+			cin >> array[i];
+		for(int j = 0; j < n; ++j)
+			cin >> a[j];
+		MyForeach(array,array+m,Print);		 
+		cout << endl;
+		MyForeach(a,a+n,Inc);		 
+		for(int i = 0;i < n; ++i)
+			cout << a[i] << ",";
+		cout << endl;
+	}
+	return 0;
+}
+输入
+多组数据
+
+每组数据第一行是两个整数 m 和 n ,都不超过 50
+
+第二行是m个不带空格的字符串
+第三行是 n个整数
+输出
+对每组数据
+第一行输出所有输入字符串连在一起的结果
+第二行输出输入中的每个整数加1的结果
+样例输入
+3 4
+Tom Mike Jack
+1 2 3 4
+1 2
+Peking
+100 200
+样例输出
+TomMikeJack
+2,3,4,5,
+Peking
+101,201,
+来源
+Guo Wei
+查看 提交 统计 提问
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+// 在此处补充你的代码
+
+template <class T,class Func>
+void MyForeach(T *begin, T *end, Func f) {
+    for (T *p = begin; p != end; p++) {
+        f(*p);
+    }
+}
+
+void Print(string s)
+{
+	cout << s;
+}
+void Inc(int & n)
+{
+	++ n;
+}
+string Array[100];
+int a[100];
+int main() {
+	int m,n;
+	while(cin >> m >> n) {
+		for(int i = 0;i < m; ++i)
+			cin >> Array[i];
+		for(int j = 0; j < n; ++j)
+			cin >> a[j];
+		MyForeach(Array,Array+m,Print);		 
+		cout << endl;
+		MyForeach(a,a+n,Inc);		 
+		for(int i = 0;i < n; ++i)
+			cout << a[i] << ",";
+		cout << endl;
+	}
+	return 0;
+}
